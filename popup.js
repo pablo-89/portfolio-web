a9820a9387034eb429efb5febeb1049ad018815d
pablo-89 <75427213+pablo-89@users.noreply.github.com>
2022-05-05 12:06:04 +0200

    arreglo de los popup y ajuste de pagina

--- popup.js
+++ popup.js
@@ -13,3 +13,33 @@ btnCerrarPopup.addEventListener('click', function() {
     popup.classList.remove('active');
 
 });
+var btnAbrirPopup = document.getElementById('btn-abrir'),
+    efecto = document.getElementById('stilo'),
+    popup = document.getElementById('pop-up'),
+    btnCerrarPopup = document.getElementById('btn-cerrar');
+
+btnAbrirPopup.addEventListener('click', function() {
+    efecto.classList.add('active');
+    popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function() {
+    efecto.classList.remove('active');
+    popup.classList.remove('active');
+
+});
+var btnAbrirPopup = document.getElementById('abrir-popup'),
+    efecto = document.getElementById('effect'),
+    popup = document.getElementById('popup1'),
+    btnCerrarPopup = document.getElementById('cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function() {
+    efecto.classList.add('active');
+    popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function() {
+    efecto.classList.remove('active');
+    popup.classList.remove('active');
+
+});
