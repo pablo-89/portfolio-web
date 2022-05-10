01ed576af466ea582c14c2c9b80f2917821a7bfa
pablo-89 <75427213+pablo-89@users.noreply.github.com>
2022-05-10 15:30:30 +0200

    ultimos cambios

--- popup.js
+++ popup.js
@@ -16,48 +16,15 @@ btnCerrarPopup.addEventListener('click', function() {
 
 
 
-var btnAbrir = document.getElementById('btn-abrir'),
-    stilo = document.getElementById('stilo'),
-    popUp = document.getElementById('pop-up'),
-    btnCerrar = document.getElementById('btn-cerrar');
-
-btnAbrir.addEventListener('click', function() {
-    stilo.classList.add('active');
-    popUp.classList.add('active');
-});
-
-btnCerrar.addEventListener('click', function() {
-    stilo.classList.remove('active');
-    popUp.classList.remove('active');
-
-});
-
-
-
-
-var AbrirPopup = document.getElementById('abrir-popup'),
-    effect = document.getElementById('effect'),
-    popup1 = document.getElementById('popup1'),
-    CerrarPopup = document.getElementById('cerrar-popup');
-
-AbrirPopup.addEventListener('click', function() {
-    effect.classList.add('active');
-    popup1.classList.add('active');
-});
-
-CerrarPopup.addEventListener('click', function() {
-    effect.classList.remove('active');
-    popup1.classList.remove('active');
-
-});
-
 
 // con esto seleccionas varios elementos con la misma clase
-var botones = document.querySelectorAll('.clasequequieras');
+var botones = document.querySelectorAll('.btn-abrir-popup');
 // con esto recorres los elementos que antes seleccionases
-botones.forEach(function(boton) {
+btnAbrirPopup.forEach(function(boton) {
     // aqui dentro de la función del forEach, se va a llamar una vez por cada elemento que haya dentro de botones, y en cada ciclo, te va a poner en la variable boton cada elemento del array
     boton.addEventListener('click', function(e) {
+        efecto.classList.add('active');
+        popup.classList.add('active');
         // aqui haces la función q se llamará al hacer click
     });
 });
